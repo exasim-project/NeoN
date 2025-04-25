0378bc6969893fa544c6a512b3bbd47f0c15addf
Gregor Olenik <gregor.olenik@web.de>
2025-04-25 18:18:06 +0200

    move volumeField implementation to src

--- include/NeoN/finiteVolume/cellCentred/fields/volumeField.hpp
+++ include/NeoN/finiteVolume/cellCentred/fields/volumeField.hpp
@@ -112,19 +112,9 @@ public:
 
     VolumeField<ValueType>& operator+=(const ValueType rhs);
 
-    VolumeField<ValueType>& operator-=(const scalar rhs)
-    {
-        sub(this->internalVector(), rhs);
-        correctBoundaryConditions();
-        return *this;
-    }
+    VolumeField<ValueType>& operator-=(const ValueType rhs);
 
-    VolumeField<ValueType>& operator+=(const scalar rhs)
-    {
-        sub(this->internalVector(), rhs);
-        correctBoundaryConditions();
-        return *this;
-    }
+    VolumeField<ValueType>& operator+=(const ValueType rhs);
 
     /**
      * @brief Corrects the boundary conditions of the surface field.
