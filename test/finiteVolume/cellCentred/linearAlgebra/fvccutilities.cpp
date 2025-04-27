b011ff57e3df22b07fb25a88e709d9f5b303fde4
Gregor Olenik <gregor.olenik@web.de>
2025-04-27 08:40:45 +0200

    assemble sparsity and add test

--- test/finiteVolume/cellCentred/linearAlgebra/fvccutilities.cpp
+++ test/finiteVolume/cellCentred/linearAlgebra/fvccutilities.cpp
@@ -23,18 +23,56 @@ TEST_CASE("Utilities")
     auto nFaces = 9;
     auto mesh = create1DUniformMesh(exec, nCells);
 
-    SECTION("Can assemble sparsity pattern from expression on " + execName)
+    SECTION("Can assemble linear system from expression on " + execName)
     {
         auto volumeBCs = fvcc::createCalculatedBCs<fvcc::VolumeBoundary<scalar>>(mesh);
         fvcc::VolumeField<scalar> phi(exec, "sf", mesh, volumeBCs);
+        fill(phi.internalVector(), 1.0);
+        fill(phi.boundaryData().value(), 1.0);
         phi.correctBoundaryConditions();
+
+        fvcc::VolumeField<scalar> rhs(exec, "sf", mesh, volumeBCs);
+        rhs.correctBoundaryConditions();
+        fill(rhs.internalVector(), 1.0);
 
         auto surfaceBCs = fvcc::createCalculatedBCs<fvcc::SurfaceBoundary<scalar>>(mesh);
         fvcc::SurfaceField<scalar> faceFlux(exec, "sf", mesh, surfaceBCs);
         fill(faceFlux.internalVector(), 1.0);
+        auto boundFaceFlux = faceFlux.internalVector().view();
+        // face on the left side has different orientation
+        parallelFor(
+            exec,
+            {mesh.nCells() - 1, mesh.nCells()},
+            KOKKOS_LAMBDA(const localIdx i) { boundFaceFlux[i] = -1.0; }
+        );
 
         Input input = TokenList({std::string("Gauss"), std::string("linear")});
-        auto div = fvcc::DivOperator(NeoN::dsl::Operator::Type::Explicit, faceFlux, phi, input);
+        auto div = fvcc::DivOperator(NeoN::dsl::Operator::Type::Implicit, faceFlux, phi, input);
+
+        auto expr = NeoN::dsl::Expression<NeoN::scalar>(exec);
+        expr.addOperator(div);
+
+        auto schemesDict = NeoN::Dictionary();
+
+        auto ls = assembleLinearSystem<NeoN::scalar>(expr, rhs, schemesDict, 0, 0.1);
+
+        auto lsHost = ls.copyToHost();
+        auto [csrHV, rhsHV] = lsHost.view();
+
+        // All values should be zero since original field was uniform
+        for (NeoN::localIdx i = 0; i < csrHV.values.size(); ++i)
+        {
+            std::cout << " csr[" << i << "]:" << csrHV.values[i] << "\n";
+        }
+        // for (NeoN::localIdx i = 0; i < csrHV.values.size(); ++i)
+        // {
+        //     REQUIRE(csrHV.values[i] == 0.0);
+        // }
+
+        for (NeoN::localIdx i = 0; i < rhsHV.size(); ++i)
+        {
+            REQUIRE(rhsHV[i] == 0.0);
+        }
     }
 }
 
