a395f4a82410766af1108abe2cffc115ba442dd3
Gregor Olenik <gregor.olenik@web.de>
2025-05-25 12:47:33 +0200

    update cellToFaceStencil test

--- test/finiteVolume/cellCentred/stencil/cellToFaceStencil.cpp
+++ test/finiteVolume/cellCentred/stencil/cellToFaceStencil.cpp
@@ -1,10 +1,11 @@
-// SPDX-License-Identifier: GPL-3.0-or-later
-// SPDX-FileCopyrightText: 2023 FoamAdapter authors
+// SPDX-License-Identifier: MIT
+// SPDX-FileCopyrightText: 2025 NeoN authors
 
 #define CATCH_CONFIG_RUNNER // Define this before including catch.hpp to create
                             // a custom main
 #include "catch2_common.hpp"
 
+#include <unordered_set>
 #include "NeoN/NeoN.hpp"
 
 namespace fvcc = NeoN::finiteVolume::cellCentred;
@@ -22,22 +23,22 @@ TEST_CASE("cell To Face Stencil")
         NeoN::SegmentedVector<NeoN::localIdx, NeoN::localIdx> stencil =
             cellToFaceStencil.computeStencil();
 
-        // auto hostStencil = stencil.copyToHost();
-        // auto stencilView = hostStencil.view();
-        //
-        // for (auto celli = 0; celli < mesh.cells().size(); celli++)
-        // {
-        //     std::unordered_set<Foam::label> faceSet;
-        //     REQUIRE(stencilView.view(celli).size() == mesh.cells()[celli].size());
-        //     for (auto facei : mesh.cells()[celli])
-        //     {
-        //         faceSet.insert(facei);
-        //     }
-        //
-        //     for (auto facei : stencilView.view(celli))
-        //     {
-        //         REQUIRE(faceSet.contains(facei));
-        //     }
-        // }
+        auto hostStencil = stencil.copyToHost();
+        auto stencilView = hostStencil.view();
+
+        for (auto celli = 0; celli < mesh.nCells(); celli++)
+        {
+            std::unordered_set<NeoN::localIdx> faceSet;
+            // REQUIRE(stencilView.view(celli).size() == mesh.cells()[celli].size());
+            // for (auto facei : mesh.cells()[celli])
+            // {
+            //     faceSet.insert(facei);
+            // }
+            //
+            // for (auto facei : stencilView.view(celli))
+            // {
+            //     REQUIRE(faceSet.contains(facei));
+            // }
+        }
     }
 }
