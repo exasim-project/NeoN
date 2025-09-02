6b2b6ec23e6580c64fa6df1eb1d079db30a46615
greole <greole@users.noreply.github.com>
2025-09-02 10:47:26 +0000

    Deploying to gh-pages from @ exasim-project/NeoN@75c14f6a647350fa8726f48f1d2a01e6e7c776ba 🚀

--- Build_PR_294/doxygen/html/search/typedefs_5.js
+++ Build_PR_294/doxygen/html/search/typedefs_5.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['globalidx_0',['globalIdx',['../namespaceNeoN.html#a6a7f5ddf0fda0ea15fd94d7bf6eed9b6',1,'NeoN']]]
+  ['fixedgradienttype_0',['FixedGradientType',['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1volumeBoundary_1_1FixedGradient.html#a6c4e12be0875c2ef23d75585f52d6fee',1,'NeoN::finiteVolume::cellCentred::volumeBoundary::FixedGradient']]]
 ];
