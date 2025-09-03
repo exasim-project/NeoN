f8a3038c229047afa7065223e34ef38281a1dff5
greole <greole@users.noreply.github.com>
2025-09-03 07:00:06 +0000

    Deploying to gh-pages from @ exasim-project/NeoN@ad39e0eb8c5ad4960c3a8c4c4f6cec78f685130c 🚀

--- latest/doxygen/html/search/variables_c.js
+++ latest/doxygen/html/search/variables_c.js
@@ -4,7 +4,8 @@ var searchData=
   ['schemedict_5f_1',['schemeDict_',['../classNeoN_1_1timeIntegration_1_1TimeIntegratorBase.html#a6ec6c1557e18be4abda8b17b673652b5',1,'NeoN::timeIntegration::TimeIntegratorBase']]],
   ['segments_2',['segments',['../classNeoN_1_1SegmentedVectorView.html#a0e101da590fc55027c7a0d0e986d27d3',1,'NeoN::SegmentedVectorView']]],
   ['solutiondict_5f_3',['solutionDict_',['../classNeoN_1_1timeIntegration_1_1TimeIntegratorBase.html#a319d692ec559c3b0863e06216769a61a',1,'NeoN::timeIntegration::TimeIntegratorBase']]],
-  ['sparsitypattern_5f_4',['sparsitypattern_',['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1DivOperatorFactory.html#a90ff60c6f54a54e4808014b857e6ae51',1,'NeoN::finiteVolume::cellCentred::DivOperatorFactory::sparsityPattern_'],['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1LaplacianOperatorFactory.html#ae307ac3e4d4b0f05dca42c7e2b379231',1,'NeoN::finiteVolume::cellCentred::LaplacianOperatorFactory::sparsityPattern_']]],
-  ['start_5f_5',['start_',['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1BoundaryPatchMixin.html#a704e4f166091d177d272ad0e55186531',1,'NeoN::finiteVolume::cellCentred::BoundaryPatchMixin']]],
-  ['subcycleindex_6',['subCycleIndex',['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1CreateFromExistingVector.html#aced149d40cd18519ea643bee33680f3f',1,'NeoN::finiteVolume::cellCentred::CreateFromExistingVector']]]
+  ['solvetime_4',['solveTime',['../structNeoN_1_1la_1_1SolverStats.html#a3ab3d77297ffe4f127da0ea54fd97674',1,'NeoN::la::SolverStats']]],
+  ['sparsitypattern_5f_5',['sparsitypattern_',['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1DivOperatorFactory.html#a90ff60c6f54a54e4808014b857e6ae51',1,'NeoN::finiteVolume::cellCentred::DivOperatorFactory::sparsityPattern_'],['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1LaplacianOperatorFactory.html#ae307ac3e4d4b0f05dca42c7e2b379231',1,'NeoN::finiteVolume::cellCentred::LaplacianOperatorFactory::sparsityPattern_']]],
+  ['start_5f_6',['start_',['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1BoundaryPatchMixin.html#a704e4f166091d177d272ad0e55186531',1,'NeoN::finiteVolume::cellCentred::BoundaryPatchMixin']]],
+  ['subcycleindex_7',['subCycleIndex',['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1CreateFromExistingVector.html#aced149d40cd18519ea643bee33680f3f',1,'NeoN::finiteVolume::cellCentred::CreateFromExistingVector']]]
 ];
