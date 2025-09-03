72eaedd898889d8977190690eb4f22a397cfee03
Gregor Olenik <gregor.olenik@web.de>
2025-09-03 07:54:45 +0200

    fixup

--- include/NeoN/finiteVolume/cellCentred/fields/volumeField.hpp
+++ include/NeoN/finiteVolume/cellCentred/fields/volumeField.hpp
@@ -112,10 +112,6 @@ public:
 
     VolumeField<ValueType>& operator+=(const ValueType rhs);
 
-    VolumeField<ValueType>& operator-=(const ValueType rhs);
-
-    VolumeField<ValueType>& operator+=(const ValueType rhs);
-
     /**
      * @brief Corrects the boundary conditions of the surface field.
      *
