ffbf507fefa9a4171ff3d24d3bd167dd3e04da0a
greole <greole@users.noreply.github.com>
2025-10-07 10:45:53 +0000

    Deploying to gh-pages from @ exasim-project/NeoN@9bf77caddc333f1662f0790aa8c07f93e10ba0ec 🚀

--- Build_PR_374/doxygen/html/search/classes_8.js
+++ Build_PR_374/doxygen/html/search/classes_8.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['neonexception_0',['NeoNException',['../classNeoN_1_1NeoNException.html',1,'NeoN']]]
+  ['neonexception_0',['NeoNException',['../classNeoN_1_1NeoNException.html',1,'NeoN']]],
+  ['noopfunctor_1',['NoOpFunctor',['../structNeoN_1_1dsl_1_1NoOpFunctor.html',1,'NeoN::dsl']]]
 ];
