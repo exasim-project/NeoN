55ed3b86fe01386519e64571fba36197f3b2fa62
greole <greole@users.noreply.github.com>
2025-10-20 15:39:26 +0000

    Deploying to gh-pages from @ exasim-project/NeoN@2bffdef479c9864229cb67e0278d3c853ca5ad11 🚀

--- Build_PR_379/doxygen/html/search/classes_9.js
+++ Build_PR_379/doxygen/html/search/classes_9.js
@@ -4,6 +4,7 @@ var searchData=
   ['oldtimedocument_1',['OldTimeDocument',['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1OldTimeDocument.html',1,'NeoN::finiteVolume::cellCentred']]],
   ['operator_2',['Operator',['../classNeoN_1_1dsl_1_1Operator.html',1,'NeoN::dsl']]],
   ['operatormixin_3',['OperatorMixin',['../classNeoN_1_1dsl_1_1OperatorMixin.html',1,'NeoN::dsl']]],
-  ['operatormixin_3c_20volumefield_3c_20valuetype_20_3e_20_3e_4',['OperatorMixin&lt; VolumeField&lt; ValueType &gt; &gt;',['../classNeoN_1_1dsl_1_1OperatorMixin.html',1,'NeoN::dsl']]],
-  ['operatormixin_3c_20volumefield_3c_20valuetype_20_3e_2c_20volumefield_3c_20scalar_20_3e_20_3e_5',['OperatorMixin&lt; VolumeField&lt; ValueType &gt;, VolumeField&lt; scalar &gt; &gt;',['../classNeoN_1_1dsl_1_1OperatorMixin.html',1,'NeoN::dsl']]]
+  ['operatormixin_3c_20vectortype_20_3e_4',['OperatorMixin&lt; VectorType &gt;',['../classNeoN_1_1dsl_1_1OperatorMixin.html',1,'NeoN::dsl']]],
+  ['operatormixin_3c_20volumefield_3c_20valuetype_20_3e_20_3e_5',['OperatorMixin&lt; VolumeField&lt; ValueType &gt; &gt;',['../classNeoN_1_1dsl_1_1OperatorMixin.html',1,'NeoN::dsl']]],
+  ['operatormixin_3c_20volumefield_3c_20valuetype_20_3e_2c_20volumefield_3c_20scalar_20_3e_20_3e_6',['OperatorMixin&lt; VolumeField&lt; ValueType &gt;, VolumeField&lt; scalar &gt; &gt;',['../classNeoN_1_1dsl_1_1OperatorMixin.html',1,'NeoN::dsl']]]
 ];
