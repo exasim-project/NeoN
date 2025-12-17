863a03e1b26d6b6eef3e8eeea7dd7e2bc9a04717
HendriceH <HendriceH@users.noreply.github.com>
2025-12-17 09:14:25 +0000

    Deploying to gh-pages from @ exasim-project/NeoN@a9f0300a6457ffbe9e4cb67b3dc30009958b8a5b 🚀

--- Build_PR_419/doxygen/html/search/all_7.js
+++ Build_PR_419/doxygen/html/search/all_7.js
@@ -3,6 +3,5 @@ var searchData=
   ['halfduplexcommbuffer_2ehpp_0',['halfDuplexCommBuffer.hpp',['../halfDuplexCommBuffer_8hpp.html',1,'']]],
   ['hasdatabase_1',['hasDatabase',['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1FieldDatabaseMixin.html#af32154beffbe1ae43b714bf44e79551a',1,'NeoN::finiteVolume::cellCentred::FieldDatabaseMixin']]],
   ['hasid_2',['hasId',['../namespaceNeoN.html#a1515bc3e21ebafd40f42151ea50ed85b',1,'NeoN']]],
-  ['hasstartup_3',['hasStartup',['../classNeoN_1_1timeIntegration_1_1DdtScheme.html#ace967abf361410ce99dc47186f952700',1,'NeoN::timeIntegration::DdtScheme']]],
-  ['hasview_4',['hasView',['../classNeoN_1_1dsl_1_1Coeff.html#ac081d961b60c3771efca69156b140d3f',1,'NeoN::dsl::Coeff']]]
+  ['hasview_3',['hasView',['../classNeoN_1_1dsl_1_1Coeff.html#ac081d961b60c3771efca69156b140d3f',1,'NeoN::dsl::Coeff']]]
 ];
