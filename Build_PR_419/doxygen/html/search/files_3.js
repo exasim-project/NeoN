8ef9b3e03e30007ad70490ae02618fe076e1830f
HendriceH <HendriceH@users.noreply.github.com>
2026-01-06 11:14:07 +0000

    Deploying to gh-pages from @ exasim-project/NeoN@ad5f80ee80e4a3146c0dea142fc5c0300d453f86 🚀

--- Build_PR_419/doxygen/html/search/files_3.js
+++ Build_PR_419/doxygen/html/search/files_3.js
@@ -2,14 +2,13 @@ var searchData=
 [
   ['database_2ehpp_0',['database.hpp',['../database_8hpp.html',1,'']]],
   ['ddt_2ehpp_1',['ddt.hpp',['../ddt_8hpp.html',1,'']]],
-  ['ddtfluxcorrboundary_2ehpp_2',['ddtFluxCorrBoundary.hpp',['../ddtFluxCorrBoundary_8hpp.html',1,'']]],
-  ['ddtoperator_2ehpp_3',['ddtOperator.hpp',['../ddtOperator_8hpp.html',1,'']]],
-  ['ddtphicorrpostassembly_2ehpp_4',['ddtPhiCorrPostAssembly.hpp',['../ddtPhiCorrPostAssembly_8hpp.html',1,'']]],
-  ['ddtscheme_2ehpp_5',['DdtScheme.hpp',['../DdtScheme_8hpp.html',1,'']]],
-  ['demangle_2ehpp_6',['demangle.hpp',['../demangle_8hpp.html',1,'']]],
-  ['diagonalsolver_2ehpp_7',['diagonalSolver.hpp',['../diagonalSolver_8hpp.html',1,'']]],
-  ['dictionary_2ehpp_8',['dictionary.hpp',['../dictionary_8hpp.html',1,'']]],
-  ['divoperator_2ehpp_9',['divOperator.hpp',['../divOperator_8hpp.html',1,'']]],
-  ['document_2ehpp_10',['document.hpp',['../document_8hpp.html',1,'']]],
-  ['domain_2ehpp_11',['domain.hpp',['../domain_8hpp.html',1,'']]]
+  ['ddtfluxcorr_2ehpp_2',['ddtFluxCorr.hpp',['../ddtFluxCorr_8hpp.html',1,'']]],
+  ['ddtfluxcorrboundary_2ehpp_3',['ddtFluxCorrBoundary.hpp',['../ddtFluxCorrBoundary_8hpp.html',1,'']]],
+  ['ddtoperator_2ehpp_4',['ddtOperator.hpp',['../ddtOperator_8hpp.html',1,'']]],
+  ['demangle_2ehpp_5',['demangle.hpp',['../demangle_8hpp.html',1,'']]],
+  ['diagonalsolver_2ehpp_6',['diagonalSolver.hpp',['../diagonalSolver_8hpp.html',1,'']]],
+  ['dictionary_2ehpp_7',['dictionary.hpp',['../dictionary_8hpp.html',1,'']]],
+  ['divoperator_2ehpp_8',['divOperator.hpp',['../divOperator_8hpp.html',1,'']]],
+  ['document_2ehpp_9',['document.hpp',['../document_8hpp.html',1,'']]],
+  ['domain_2ehpp_10',['domain.hpp',['../domain_8hpp.html',1,'']]]
 ];
