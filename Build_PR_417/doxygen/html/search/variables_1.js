214929029d35a77c5d8543453ae2879889bf0a77
greole <greole@users.noreply.github.com>
2026-01-06 20:50:43 +0000

    Deploying to gh-pages from @ exasim-project/NeoN@aa7f01dd56ad3c9b40fa308e1ce1498a32b6e725 🚀

--- Build_PR_417/doxygen/html/search/variables_1.js
+++ Build_PR_417/doxygen/html/search/variables_1.js
@@ -1,4 +1,6 @@
 var searchData=
 [
-  ['base_0',['Base',['../classNeoN_1_1RuntimeSelectionFactory_3_01Base_00_01Parameters_3_01Args_8_8_8_01_4_01_4.html#adf57496f049f97afcdca5262eb51590f',1,'NeoN::RuntimeSelectionFactory&lt; Base, Parameters&lt; Args... &gt; &gt;']]]
+  ['base_0',['Base',['../classNeoN_1_1RuntimeSelectionFactory_3_01Base_00_01Parameters_3_01Args_8_8_8_01_4_01_4.html#adf57496f049f97afcdca5262eb51590f',1,'NeoN::RuntimeSelectionFactory&lt; Base, Parameters&lt; Args... &gt; &gt;']]],
+  ['boundarymatrix_1',['boundaryMatrix',['../structNeoN_1_1la_1_1LinearSystemView.html#a75c4dfa737ff000013a3ce45183fa8b9',1,'NeoN::la::LinearSystemView']]],
+  ['boundaryrhs_2',['boundaryRhs',['../structNeoN_1_1la_1_1LinearSystemView.html#a997b4a21f91015df03a7da02607977a5',1,'NeoN::la::LinearSystemView']]]
 ];
