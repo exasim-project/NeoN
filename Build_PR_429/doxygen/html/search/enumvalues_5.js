ccee4d02d238dcb673253447fe2c7f1bdb814ded
greole <greole@users.noreply.github.com>
2026-01-09 10:58:57 +0000

    Deploying to gh-pages from @ exasim-project/NeoN@d18d741609b78a5eef7d8739d221de89ccdd5205 🚀

---
+++ Build_PR_429/doxygen/html/search/enumvalues_5.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['implicit_0',['Implicit',['../classNeoN_1_1dsl_1_1Operator.html#ad72823c9a2d04641ab7c7181902f6dd3a07e01d7a96d9fd76cf08aa5916f7c788',1,'NeoN::dsl::Operator']]],
+  ['info_1',['Info',['../namespaceNeoN_1_1Logging.html#a8cfeee86930e70637e61a759b7361214a207edd6969a7322a9f59a8c8eafb8670',1,'NeoN::Logging']]]
+];
---
+++ Build_PR_429/doxygen/html/search/variables_a.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['name_0',['name',['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1CreateFromExistingVector.html#ae8f9c3ba621f5ff0cf743ad317090983',1,'NeoN::finiteVolume::cellCentred::CreateFromExistingVector::name'],['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1DomainMixin.html#adccbbd2c1b3d558ca69c4db20e2f9392',1,'NeoN::finiteVolume::cellCentred::DomainMixin::name']]],
+  ['name_5f_1',['name_',['../classNeoN_1_1CollectionMixin.html#a019b4a98e1b7c10e7657bf5922a0ca3c',1,'NeoN::CollectionMixin']]],
+  ['nl_2',['NL',['../time_8hpp.html#a6c2bdfd6f25ccadb92fcff4173b7fec2',1,'time.hpp']]],
+  ['numiter_3',['numIter',['../structNeoN_1_1la_1_1SolverStats.html#a1c1ed94a5cb51273fc760ac035317625',1,'NeoN::la::SolverStats']]]
+];
