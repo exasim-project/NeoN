761e739ff464402339bd70e93d25f39d41a6c331
Gregor Olenik <gregor.olenik@web.de>
2026-01-12 14:33:49 +0100

    add unit tests again

--- include/NeoN/linearAlgebra/CSRMatrix.hpp
+++ include/NeoN/linearAlgebra/CSRMatrix.hpp
@@ -199,7 +199,7 @@ public:
      * @brief Get a reference to column indices vector.
      * @return Vector containing the column indices.
      */
-    [[nodiscard]] std::shared_ptr<const SparsityPattern<IndexType>> sparsity()
+    [[nodiscard]] std::shared_ptr<const SparsityPattern<IndexType>> sparsity() const
     {
         return sparsityPattern_;
     }
@@ -256,12 +256,28 @@ public:
      */
     void negLUx(const Vector<ValueType>& a, Vector<ValueType>& out) const;
 
+    /* @brief computes out = P - L*f + U*f
+     *
+     * @notes explicitly sets out values to zero
+     */
+    // void PLUx(const Vector<ValueType>& a, Vector<ValueType>& out) const;
+
 private:
 
     Vector<ValueType> values_; //!< The (non-zero) values of the CSR matrix.
 
     std::shared_ptr<const SparsityPattern<IndexType>> sparsityPattern_;
 };
+
+
+// FIXME
+// template<typename ValueType, typename IndexType>
+// void faceIterateNegLUx(
+//         const CSRMatrix<ValueType, IndexType>& mat,
+//         const MatrixIterator& mi,
+//         const Vector<ValueType>& a,
+//         Vector<ValueType>& out
+//     ) const;
 
 // /* @brief given a csr matrix this function copies the matrix and converts to requested target
 // types
