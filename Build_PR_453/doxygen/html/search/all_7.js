62c4727db04737f15e85c5ec43a5aefae701394e
greole <greole@users.noreply.github.com>
2026-02-07 22:37:34 +0000

    Deploying to gh-pages from @ exasim-project/NeoN@de89b108262b1ba869cec8ad30c65cf188a3a74c 🚀

--- Build_PR_453/doxygen/html/search/all_7.js
+++ Build_PR_453/doxygen/html/search/all_7.js
@@ -3,6 +3,7 @@ var searchData=
   ['halfduplexcommbuffer_2ehpp_0',['halfDuplexCommBuffer.hpp',['../halfDuplexCommBuffer_8hpp.html',1,'']]],
   ['hasdatabase_1',['hasDatabase',['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1FieldDatabaseMixin.html#af32154beffbe1ae43b714bf44e79551a',1,'NeoN::finiteVolume::cellCentred::FieldDatabaseMixin']]],
   ['hasid_2',['hasId',['../namespaceNeoN.html#a1515bc3e21ebafd40f42151ea50ed85b',1,'NeoN']]],
-  ['hasoperator_3',['hasOperator',['../classNeoN_1_1dsl_1_1Expression.html#a734b4b6fc764981251048dff272caabb',1,'NeoN::dsl::Expression']]],
-  ['hasview_4',['hasView',['../classNeoN_1_1dsl_1_1Coeff.html#ac081d961b60c3771efca69156b140d3f',1,'NeoN::dsl::Coeff']]]
+  ['hasoperator_3',['hasOperator',['../classNeoN_1_1dsl_1_1Expression.html#a74dd5dfc39a01b0b748d47db287f063a',1,'NeoN::dsl::Expression']]],
+  ['hasoperatoroftype_4',['hasOperatorOfType',['../classNeoN_1_1dsl_1_1Expression.html#a0f72ac4fa0fd9d5213c1919c3a347b99',1,'NeoN::dsl::Expression']]],
+  ['hasview_5',['hasView',['../classNeoN_1_1dsl_1_1Coeff.html#ac081d961b60c3771efca69156b140d3f',1,'NeoN::dsl::Coeff']]]
 ];
