ac6bfbdaab587a772e88a9e56ce773d871b15170
greole <greole@users.noreply.github.com>
2026-02-25 12:37:36 +0000

    Deploying to gh-pages from @ exasim-project/NeoN@f8d65ffebf2539f6743402f6e1592c41493cb147 🚀

--- Build_PR_452/doxygen/html/search/classes_2.js
+++ Build_PR_452/doxygen/html/search/classes_2.js
@@ -1,12 +1,14 @@
 var searchData=
 [
   ['calculated_0',['calculated',['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1surfaceBoundary_1_1Calculated.html',1,'NeoN::finiteVolume::cellCentred::surfaceBoundary::Calculated&lt; ValueType &gt;'],['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1volumeBoundary_1_1Calculated.html',1,'NeoN::finiteVolume::cellCentred::volumeBoundary::Calculated&lt; ValueType &gt;']]],
-  ['celltofacestencil_1',['CellToFaceStencil',['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1CellToFaceStencil.html',1,'NeoN::finiteVolume::cellCentred']]],
-  ['coeff_2',['Coeff',['../classNeoN_1_1dsl_1_1Coeff.html',1,'NeoN::dsl']]],
-  ['collection_3',['Collection',['../classNeoN_1_1Collection.html',1,'NeoN']]],
-  ['collectionmixin_4',['CollectionMixin',['../classNeoN_1_1CollectionMixin.html',1,'NeoN']]],
-  ['collectionmixin_3c_20oldtimedocument_20_3e_5',['CollectionMixin&lt; OldTimeDocument &gt;',['../classNeoN_1_1CollectionMixin.html',1,'NeoN']]],
-  ['collectionmixin_3c_20vectordocument_20_3e_6',['CollectionMixin&lt; VectorDocument &gt;',['../classNeoN_1_1CollectionMixin.html',1,'NeoN']]],
-  ['cpuexecutor_7',['CPUExecutor',['../classNeoN_1_1CPUExecutor.html',1,'NeoN']]],
-  ['createfromexistingvector_8',['CreateFromExistingVector',['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1CreateFromExistingVector.html',1,'NeoN::finiteVolume::cellCentred']]]
+  ['cellbaseddata_1',['CellBasedData',['../structNeoN_1_1la_1_1CellBasedIterator_1_1CellBasedData.html',1,'NeoN::la::CellBasedIterator']]],
+  ['cellbasediterator_2',['CellBasedIterator',['../classNeoN_1_1la_1_1CellBasedIterator.html',1,'NeoN::la']]],
+  ['celltofacestencil_3',['CellToFaceStencil',['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1CellToFaceStencil.html',1,'NeoN::finiteVolume::cellCentred']]],
+  ['coeff_4',['Coeff',['../classNeoN_1_1dsl_1_1Coeff.html',1,'NeoN::dsl']]],
+  ['collection_5',['Collection',['../classNeoN_1_1Collection.html',1,'NeoN']]],
+  ['collectionmixin_6',['CollectionMixin',['../classNeoN_1_1CollectionMixin.html',1,'NeoN']]],
+  ['collectionmixin_3c_20oldtimedocument_20_3e_7',['CollectionMixin&lt; OldTimeDocument &gt;',['../classNeoN_1_1CollectionMixin.html',1,'NeoN']]],
+  ['collectionmixin_3c_20vectordocument_20_3e_8',['CollectionMixin&lt; VectorDocument &gt;',['../classNeoN_1_1CollectionMixin.html',1,'NeoN']]],
+  ['cpuexecutor_9',['CPUExecutor',['../classNeoN_1_1CPUExecutor.html',1,'NeoN']]],
+  ['createfromexistingvector_10',['CreateFromExistingVector',['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1CreateFromExistingVector.html',1,'NeoN::finiteVolume::cellCentred']]]
 ];
--- Build_PR_452/doxygen/html/search/variables_2.js
+++ Build_PR_452/doxygen/html/search/variables_2.js
@@ -1,8 +1,10 @@
 var searchData=
 [
   ['c_0',['c',['../structNeoN_1_1detail_1_1RefHolder.html#a19683965bd28878cf74e39159a9c8102',1,'NeoN::detail::RefHolder']]],
-  ['coeffs_5f_1',['coeffs_',['../classNeoN_1_1dsl_1_1OperatorMixin.html#aca2e5d4a971d6f5e412d9c3466f0890c',1,'NeoN::dsl::OperatorMixin']]],
-  ['colidxs_2',['colIdxs',['../structNeoN_1_1la_1_1SparsityView.html#a1cb5b4b48a9ed148cf8b403dac414c0e',1,'NeoN::la::SparsityView']]],
-  ['control_5fdict_5fname_3',['CONTROL_DICT_NAME',['../classNeoN_1_1Time.html#ad5a35398d9c27636b32a69aa19db11e2',1,'NeoN::Time']]],
-  ['creationts_4',['creationTS',['../classNeoN_1_1Logging_1_1LogEvent.html#ae08d532e07e1a1403f395756b2d07a68',1,'NeoN::Logging::LogEvent']]]
+  ['cellbasediteratordata_5f_1',['cellBasedIteratorData_',['../classNeoN_1_1la_1_1CellBasedIterator.html#a4017259e8843187c956e846ae4ebcc47',1,'NeoN::la::CellBasedIterator']]],
+  ['cellfaces_2',['cellFaces',['../structNeoN_1_1la_1_1CellBasedIterator_1_1CellBasedData.html#a30829c4df80afa9bfe9ebf5993d6141b',1,'NeoN::la::CellBasedIterator::CellBasedData']]],
+  ['coeffs_5f_3',['coeffs_',['../classNeoN_1_1dsl_1_1OperatorMixin.html#aca2e5d4a971d6f5e412d9c3466f0890c',1,'NeoN::dsl::OperatorMixin']]],
+  ['colidxs_4',['colIdxs',['../structNeoN_1_1la_1_1SparsityView.html#a1cb5b4b48a9ed148cf8b403dac414c0e',1,'NeoN::la::SparsityView']]],
+  ['control_5fdict_5fname_5',['CONTROL_DICT_NAME',['../classNeoN_1_1Time.html#ad5a35398d9c27636b32a69aa19db11e2',1,'NeoN::Time']]],
+  ['creationts_6',['creationTS',['../classNeoN_1_1Logging_1_1LogEvent.html#ae08d532e07e1a1403f395756b2d07a68',1,'NeoN::Logging::LogEvent']]]
 ];
