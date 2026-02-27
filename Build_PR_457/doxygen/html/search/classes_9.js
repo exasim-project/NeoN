b9df624484f414bfcbd441c06f455e446898171d
HenningScheufler <HenningScheufler@users.noreply.github.com>
2026-02-27 12:37:42 +0000

    Deploying to gh-pages from @ exasim-project/NeoN@2dcc37b27d02a2b50b9695cb994ace622ad70b1a 🚀

--- Build_PR_457/doxygen/html/search/classes_9.js
+++ Build_PR_457/doxygen/html/search/classes_9.js
@@ -1,10 +1,4 @@
 var searchData=
 [
-  ['oldtimecollection_0',['OldTimeCollection',['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1OldTimeCollection.html',1,'NeoN::finiteVolume::cellCentred']]],
-  ['oldtimedocument_1',['OldTimeDocument',['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1OldTimeDocument.html',1,'NeoN::finiteVolume::cellCentred']]],
-  ['operator_2',['Operator',['../classNeoN_1_1dsl_1_1Operator.html',1,'NeoN::dsl']]],
-  ['operatormixin_3',['OperatorMixin',['../classNeoN_1_1dsl_1_1OperatorMixin.html',1,'NeoN::dsl']]],
-  ['operatormixin_3c_20vectortype_20_3e_4',['OperatorMixin&lt; VectorType &gt;',['../classNeoN_1_1dsl_1_1OperatorMixin.html',1,'NeoN::dsl']]],
-  ['operatormixin_3c_20volumefield_3c_20valuetype_20_3e_20_3e_5',['OperatorMixin&lt; VolumeField&lt; ValueType &gt; &gt;',['../classNeoN_1_1dsl_1_1OperatorMixin.html',1,'NeoN::dsl']]],
-  ['operatormixin_3c_20volumefield_3c_20valuetype_20_3e_2c_20volumefield_3c_20scalar_20_3e_20_3e_6',['OperatorMixin&lt; VolumeField&lt; ValueType &gt;, VolumeField&lt; scalar &gt; &gt;',['../classNeoN_1_1dsl_1_1OperatorMixin.html',1,'NeoN::dsl']]]
+  ['neonexception_0',['NeoNException',['../classNeoN_1_1NeoNException.html',1,'NeoN']]]
 ];
