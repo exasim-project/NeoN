61bc342a196a9bd707b34817f521c1b5c076091c
dheerajraghunathan <dheerajraghunathan@users.noreply.github.com>
2026-03-02 17:02:40 +0000

    Deploying to gh-pages from @ exasim-project/NeoN@96378de14760a2cb08efd29061524a5a05a8a08a 🚀

--- Build_PR_459/doxygen/html/search/classes_8.js
+++ Build_PR_459/doxygen/html/search/classes_8.js
@@ -1,6 +1,10 @@
 var searchData=
 [
-  ['matrix_0',['Matrix',['../classNeoN_1_1la_1_1Matrix.html',1,'NeoN::la']]],
-  ['matrix_3c_20valuetype_2c_20localidx_20_3e_1',['Matrix&lt; ValueType, localIdx &gt;',['../classNeoN_1_1la_1_1Matrix.html',1,'NeoN::la']]],
-  ['matrixview_2',['MatrixView',['../structNeoN_1_1la_1_1MatrixView.html',1,'NeoN::la']]]
+  ['laplacianoperator_0',['LaplacianOperator',['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1LaplacianOperator.html',1,'NeoN::finiteVolume::cellCentred']]],
+  ['laplacianoperatorfactory_1',['LaplacianOperatorFactory',['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1LaplacianOperatorFactory.html',1,'NeoN::finiteVolume::cellCentred']]],
+  ['linear_2',['Linear',['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1Linear.html',1,'NeoN::finiteVolume::cellCentred']]],
+  ['linearsystem_3',['LinearSystem',['../classNeoN_1_1la_1_1LinearSystem.html',1,'NeoN::la']]],
+  ['linearsystemview_4',['LinearSystemView',['../structNeoN_1_1la_1_1LinearSystemView.html',1,'NeoN::la']]],
+  ['logevent_5',['LogEvent',['../classNeoN_1_1Logging_1_1LogEvent.html',1,'NeoN::Logging']]],
+  ['logger_6',['Logger',['../classNeoN_1_1Logging_1_1Logger.html',1,'NeoN::Logging']]]
 ];
