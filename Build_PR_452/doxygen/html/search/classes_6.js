af5a1182218c733878e4f10704325b80b0aabe18
greole <greole@users.noreply.github.com>
2026-03-06 23:03:56 +0000

    Deploying to gh-pages from @ exasim-project/NeoN@595c00902b240216d7e29f73f86ce15eefdeefe6 🚀

--- Build_PR_452/doxygen/html/search/classes_6.js
+++ Build_PR_452/doxygen/html/search/classes_6.js
@@ -1,12 +1,13 @@
 var searchData=
 [
-  ['gaussgreendiv_0',['GaussGreenDiv',['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1GaussGreenDiv.html',1,'NeoN::finiteVolume::cellCentred']]],
-  ['gaussgreendivlaplacian_1',['GaussGreenDivLaplacian',['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1GaussGreenDivLaplacian.html',1,'NeoN::finiteVolume::cellCentred']]],
-  ['gaussgreengrad_2',['GaussGreenGrad',['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1GaussGreenGrad.html',1,'NeoN::finiteVolume::cellCentred']]],
-  ['gaussgreenlaplacian_3',['GaussGreenLaplacian',['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1GaussGreenLaplacian.html',1,'NeoN::finiteVolume::cellCentred']]],
-  ['geometryscheme_4',['GeometryScheme',['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1GeometryScheme.html',1,'NeoN::finiteVolume::cellCentred']]],
-  ['geometryschemefactory_5',['GeometrySchemeFactory',['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1GeometrySchemeFactory.html',1,'NeoN::finiteVolume::cellCentred']]],
-  ['gpuexecutor_6',['GPUExecutor',['../classNeoN_1_1GPUExecutor.html',1,'NeoN']]],
-  ['gradoperator_7',['GradOperator',['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1GradOperator.html',1,'NeoN::finiteVolume::cellCentred']]],
-  ['gradoperatorfactory_8',['GradOperatorFactory',['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1GradOperatorFactory.html',1,'NeoN::finiteVolume::cellCentred']]]
+  ['gaussgreenddtdivlaplacian_0',['GaussGreenDdtDivLaplacian',['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1GaussGreenDdtDivLaplacian.html',1,'NeoN::finiteVolume::cellCentred']]],
+  ['gaussgreendiv_1',['GaussGreenDiv',['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1GaussGreenDiv.html',1,'NeoN::finiteVolume::cellCentred']]],
+  ['gaussgreendivlaplacian_2',['GaussGreenDivLaplacian',['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1GaussGreenDivLaplacian.html',1,'NeoN::finiteVolume::cellCentred']]],
+  ['gaussgreengrad_3',['GaussGreenGrad',['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1GaussGreenGrad.html',1,'NeoN::finiteVolume::cellCentred']]],
+  ['gaussgreenlaplacian_4',['GaussGreenLaplacian',['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1GaussGreenLaplacian.html',1,'NeoN::finiteVolume::cellCentred']]],
+  ['geometryscheme_5',['GeometryScheme',['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1GeometryScheme.html',1,'NeoN::finiteVolume::cellCentred']]],
+  ['geometryschemefactory_6',['GeometrySchemeFactory',['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1GeometrySchemeFactory.html',1,'NeoN::finiteVolume::cellCentred']]],
+  ['gpuexecutor_7',['GPUExecutor',['../classNeoN_1_1GPUExecutor.html',1,'NeoN']]],
+  ['gradoperator_8',['GradOperator',['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1GradOperator.html',1,'NeoN::finiteVolume::cellCentred']]],
+  ['gradoperatorfactory_9',['GradOperatorFactory',['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1GradOperatorFactory.html',1,'NeoN::finiteVolume::cellCentred']]]
 ];
--- Build_PR_452/doxygen/html/search/files_6.js
+++ Build_PR_452/doxygen/html/search/files_6.js
@@ -1,11 +1,12 @@
 var searchData=
 [
-  ['gaussgreendiv_2ehpp_0',['gaussGreenDiv.hpp',['../gaussGreenDiv_8hpp.html',1,'']]],
-  ['gaussgreendivlaplacian_2ehpp_1',['gaussGreenDivLaplacian.hpp',['../gaussGreenDivLaplacian_8hpp.html',1,'']]],
-  ['gaussgreengrad_2ehpp_2',['gaussGreenGrad.hpp',['../gaussGreenGrad_8hpp.html',1,'']]],
-  ['gaussgreenlaplacian_2ehpp_3',['gaussGreenLaplacian.hpp',['../gaussGreenLaplacian_8hpp.html',1,'']]],
-  ['geometryscheme_2ehpp_4',['geometryScheme.hpp',['../geometryScheme_8hpp.html',1,'']]],
-  ['ginkgo_2ehpp_5',['ginkgo.hpp',['../ginkgo_8hpp.html',1,'']]],
-  ['gpuexecutor_2ehpp_6',['GPUExecutor.hpp',['../GPUExecutor_8hpp.html',1,'']]],
-  ['gradoperator_2ehpp_7',['gradOperator.hpp',['../gradOperator_8hpp.html',1,'']]]
+  ['gaussgreenddtdivlaplacian_2ehpp_0',['gaussGreenDdtDivLaplacian.hpp',['../gaussGreenDdtDivLaplacian_8hpp.html',1,'']]],
+  ['gaussgreendiv_2ehpp_1',['gaussGreenDiv.hpp',['../gaussGreenDiv_8hpp.html',1,'']]],
+  ['gaussgreendivlaplacian_2ehpp_2',['gaussGreenDivLaplacian.hpp',['../gaussGreenDivLaplacian_8hpp.html',1,'']]],
+  ['gaussgreengrad_2ehpp_3',['gaussGreenGrad.hpp',['../gaussGreenGrad_8hpp.html',1,'']]],
+  ['gaussgreenlaplacian_2ehpp_4',['gaussGreenLaplacian.hpp',['../gaussGreenLaplacian_8hpp.html',1,'']]],
+  ['geometryscheme_2ehpp_5',['geometryScheme.hpp',['../geometryScheme_8hpp.html',1,'']]],
+  ['ginkgo_2ehpp_6',['ginkgo.hpp',['../ginkgo_8hpp.html',1,'']]],
+  ['gpuexecutor_2ehpp_7',['GPUExecutor.hpp',['../GPUExecutor_8hpp.html',1,'']]],
+  ['gradoperator_2ehpp_8',['gradOperator.hpp',['../gradOperator_8hpp.html',1,'']]]
 ];
