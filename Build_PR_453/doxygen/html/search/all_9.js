a4befe738c16fa553abd5f6aee80ed4b499da500
greole <greole@users.noreply.github.com>
2026-03-09 15:20:39 +0000

    Deploying to gh-pages from @ exasim-project/NeoN@a37b707d77fe30465656a5bd3e793302dbb06ac3 🚀

--- Build_PR_453/doxygen/html/search/all_9.js
+++ Build_PR_453/doxygen/html/search/all_9.js
@@ -1,5 +1,8 @@
 var searchData=
 [
   ['key_0',['key',['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1FieldDatabaseMixin.html#ab1ea0a9e16f0d7fdfe345d755b069d30',1,'NeoN::finiteVolume::cellCentred::FieldDatabaseMixin']]],
-  ['keys_1',['keys',['../classNeoN_1_1Dictionary.html#ab7a9dfa55b6449147a8115f107771e88',1,'NeoN::Dictionary']]]
+  ['keys_1',['keys',['../classNeoN_1_1Dictionary.html#ab7a9dfa55b6449147a8115f107771e88',1,'NeoN::Dictionary']]],
+  ['kokkos_2ehpp_2',['kokkos.hpp',['../kokkos_8hpp.html',1,'']]],
+  ['kokkosallocactor_3',['KokkosAllocactor',['../classKokkosAllocactor.html',1,'']]],
+  ['kokkosallocator_4',['KokkosAllocator',['../classNeoN_1_1KokkosAllocator.html',1,'NeoN']]]
 ];
--- Build_PR_453/doxygen/html/search/files_c.js
+++ Build_PR_453/doxygen/html/search/files_c.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['parallelalgorithms_2ehpp_0',['parallelAlgorithms.hpp',['../parallelAlgorithms_8hpp.html',1,'']]],
-  ['petsc_2ehpp_1',['petsc.hpp',['../petsc_8hpp.html',1,'']]],
-  ['petscsolvercontext_2ehpp_2',['petscSolverContext.hpp',['../petscSolverContext_8hpp.html',1,'']]]
+  ['oldtimecollection_2ehpp_0',['oldTimeCollection.hpp',['../oldTimeCollection_8hpp.html',1,'']]],
+  ['operator_2ehpp_1',['operator.hpp',['../operator_8hpp.html',1,'']]],
+  ['operators_2ehpp_2',['operators.hpp',['../operators_8hpp.html',1,'']]]
 ];
