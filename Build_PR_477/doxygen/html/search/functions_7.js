d2c686e7d0884259286512ce65c2d45bc28456a3
HendriceH <HendriceH@users.noreply.github.com>
2026-03-31 11:33:29 +0000

    Deploying to gh-pages from @ exasim-project/NeoN@0829c1bf12de8f15d257b23aa50fc272345758f5 🚀

--- Build_PR_477/doxygen/html/search/functions_7.js
+++ Build_PR_477/doxygen/html/search/functions_7.js
@@ -2,7 +2,8 @@ var searchData=
 [
   ['hasdatabase_0',['hasDatabase',['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1FieldDatabaseMixin.html#af32154beffbe1ae43b714bf44e79551a',1,'NeoN::finiteVolume::cellCentred::FieldDatabaseMixin']]],
   ['hasid_1',['hasId',['../namespaceNeoN.html#a1515bc3e21ebafd40f42151ea50ed85b',1,'NeoN']]],
-  ['hasoperator_2',['hasOperator',['../classNeoN_1_1dsl_1_1Expression.html#a92424d6e14a1aa5f25db658407a136c3',1,'NeoN::dsl::Expression']]],
-  ['hasoperatoroftype_3',['hasOperatorOfType',['../classNeoN_1_1dsl_1_1Expression.html#a270a19070e2129d7b1eb986a47c4b99c',1,'NeoN::dsl::Expression']]],
-  ['hasview_4',['hasView',['../classNeoN_1_1dsl_1_1Coeff.html#ac081d961b60c3771efca69156b140d3f',1,'NeoN::dsl::Coeff']]]
+  ['hasimplicitcorrection_2',['hasimplicitcorrection',['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1Corrected.html#a510d88dc0774e63c49a60d2c309ecfa1',1,'NeoN::finiteVolume::cellCentred::Corrected::hasImplicitCorrection()'],['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1FaceNormalGradientFactory.html#ab87c07e3b2d4b0d69f08ae8418b906f2',1,'NeoN::finiteVolume::cellCentred::FaceNormalGradientFactory::hasImplicitCorrection()'],['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1FaceNormalGradient.html#a151b6dbb72b1f4ecacb151240d657f9b',1,'NeoN::finiteVolume::cellCentred::FaceNormalGradient::hasImplicitCorrection()'],['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1LimitedCorrected.html#a469be6f818737c0d56e16e20d5f9c494',1,'NeoN::finiteVolume::cellCentred::LimitedCorrected::hasImplicitCorrection()']]],
+  ['hasoperator_3',['hasOperator',['../classNeoN_1_1dsl_1_1Expression.html#a92424d6e14a1aa5f25db658407a136c3',1,'NeoN::dsl::Expression']]],
+  ['hasoperatoroftype_4',['hasOperatorOfType',['../classNeoN_1_1dsl_1_1Expression.html#a270a19070e2129d7b1eb986a47c4b99c',1,'NeoN::dsl::Expression']]],
+  ['hasview_5',['hasView',['../classNeoN_1_1dsl_1_1Coeff.html#ac081d961b60c3771efca69156b140d3f',1,'NeoN::dsl::Coeff']]]
 ];
