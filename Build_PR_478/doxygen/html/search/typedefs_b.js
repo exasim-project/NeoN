b1e8795d92e5d2dc1d80fd3fcdd5c767cf866adb
HendriceH <HendriceH@users.noreply.github.com>
2026-04-01 09:46:44 +0000

    Deploying to gh-pages from @ exasim-project/NeoN@56c6b4356f019983b13af68ec0e8d671770c0503 🚀

---
+++ Build_PR_478/doxygen/html/search/typedefs_b.js
@@ -0,0 +1,9 @@
+var searchData=
+[
+  ['scalar_0',['scalar',['../namespaceNeoN.html#a93c8d00d3aa9fc6f9271c4c0b4f0da97',1,'NeoN']]],
+  ['scalarvector_1',['scalarVector',['../namespaceNeoN.html#ad4b93cc6d70df72f518f4b16fb19810f',1,'NeoN']]],
+  ['size_5ft_2',['size_t',['../namespaceNeoN.html#a5ea7bf3a45fbcf9c6ee454474ab13c06',1,'NeoN']]],
+  ['sparsityindextype_3',['sparsityindextype',['../classNeoN_1_1la_1_1SparsityPattern.html#aa85ef66c196ad25925c03b32f514ac0a',1,'NeoN::la::SparsityPattern::SparsityIndexType'],['../classNeoN_1_1la_1_1CooSparsityPattern.html#af510ff94ab03a293a7a926b695218cdc',1,'NeoN::la::CooSparsityPattern::SparsityIndexType']]],
+  ['surfscalarfield_4',['SurfScalarField',['../namespaceNeoN_1_1finiteVolume_1_1cellCentred.html#ab2724f0e93a4ccda728971a7922e4d92',1,'NeoN::finiteVolume::cellCentred']]],
+  ['symmetrytype_5',['SymmetryType',['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1volumeBoundary_1_1Symmetry.html#ad127393f5c6fc53bc41e3148c2b63bc9',1,'NeoN::finiteVolume::cellCentred::volumeBoundary::Symmetry']]]
+];
