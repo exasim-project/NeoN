657802cec70101daf2003e5c194756a4c992f1d3
greole <greole@users.noreply.github.com>
2026-04-08 14:01:33 +0000

    Deploying to gh-pages from @ exasim-project/NeoN@308a5e6bdd4d6835f0fe63f3777258d3e1784b33 🚀

--- Build_PR_414/doxygen/html/search/variables_2.js
+++ Build_PR_414/doxygen/html/search/variables_2.js
@@ -2,7 +2,6 @@ var searchData=
 [
   ['coeffs_5f_0',['coeffs_',['../classNeoN_1_1dsl_1_1OperatorMixin.html#aca2e5d4a971d6f5e412d9c3466f0890c',1,'NeoN::dsl::OperatorMixin']]],
   ['colidxs_1',['colIdxs',['../structNeoN_1_1la_1_1SparsityView.html#a1cb5b4b48a9ed148cf8b403dac414c0e',1,'NeoN::la::SparsityView']]],
-  ['commidx_2',['commIdx',['../structNeoN_1_1CommunicationPattern.html#a489efcd9ddfdf298070fbf3881f58498',1,'NeoN::CommunicationPattern']]],
-  ['control_5fdict_5fname_3',['CONTROL_DICT_NAME',['../classNeoN_1_1Time.html#ad5a35398d9c27636b32a69aa19db11e2',1,'NeoN::Time']]],
-  ['creationts_4',['creationTS',['../classNeoN_1_1Logging_1_1LogEvent.html#ae08d532e07e1a1403f395756b2d07a68',1,'NeoN::Logging::LogEvent']]]
+  ['control_5fdict_5fname_2',['CONTROL_DICT_NAME',['../classNeoN_1_1Time.html#ad5a35398d9c27636b32a69aa19db11e2',1,'NeoN::Time']]],
+  ['creationts_3',['creationTS',['../classNeoN_1_1Logging_1_1LogEvent.html#ae08d532e07e1a1403f395756b2d07a68',1,'NeoN::Logging::LogEvent']]]
 ];
