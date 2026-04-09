7df19ad068a1f58c936a809d03292af1a81d1eb4
HendriceH <HendriceH@users.noreply.github.com>
2026-04-09 15:43:25 +0000

    Deploying to gh-pages from @ exasim-project/NeoN@5584e51ac5803d219435f970be362c1d738bb93c 🚀

---
+++ Build_PR_482/doxygen/html/search/classes_7.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['kokkosallocactor_0',['KokkosAllocactor',['../classKokkosAllocactor.html',1,'']]],
+  ['kokkosallocator_1',['KokkosAllocator',['../classNeoN_1_1KokkosAllocator.html',1,'NeoN']]]
+];
