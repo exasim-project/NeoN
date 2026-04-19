30f8596622cb63db306c92f0bf516aab4fae0757
HendriceH <HendriceH@users.noreply.github.com>
2026-04-19 19:27:55 +0000

    Deploying to gh-pages from @ exasim-project/NeoN@0ebf5103c19e481ad3c20800527f1aa64bd50af8 🚀

---
+++ Build_PR_489/doxygen/html/search/variables_1.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['base_0',['Base',['../classNeoN_1_1RuntimeSelectionFactory_3_01Base_00_01Parameters_3_01Args_8_8_8_01_4_01_4.html#adf57496f049f97afcdca5262eb51590f',1,'NeoN::RuntimeSelectionFactory&lt; Base, Parameters&lt; Args... &gt; &gt;']]],
+  ['boundarymapvector_1',['boundaryMapVector',['../structNeoN_1_1CommunicationPattern.html#aba8dc4dfb133a8cb7820f3a002e0678b',1,'NeoN::CommunicationPattern']]],
+  ['boundarymatrix_2',['boundaryMatrix',['../structNeoN_1_1la_1_1LinearSystemView.html#ad3d767cbed3c9eb8fbbfccde52f055ad',1,'NeoN::la::LinearSystemView']]],
+  ['boundaryrhs_3',['boundaryRhs',['../structNeoN_1_1la_1_1LinearSystemView.html#a878e591e4afb9747bb1be486ea6b3c3c',1,'NeoN::la::LinearSystemView']]]
+];
