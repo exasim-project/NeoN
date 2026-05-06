e3c6fdef3b4e7583f9648f133d3635f213552319
greole <greole@users.noreply.github.com>
2026-05-06 19:47:11 +0000

    Deploying to gh-pages from @ exasim-project/NeoN@a3635b25bf0f3c501f383a315fb4bdac943b6203 🚀

--- Build_PR_486/doxygen/html/search/classes_9.js
+++ Build_PR_486/doxygen/html/search/classes_9.js
@@ -1,7 +1,8 @@
 var searchData=
 [
   ['matrix_0',['Matrix',['../classNeoN_1_1la_1_1Matrix.html',1,'NeoN::la']]],
-  ['matrix_3c_20valuetype_2c_20localidx_20_3e_1',['Matrix&lt; ValueType, localIdx &gt;',['../classNeoN_1_1la_1_1Matrix.html',1,'NeoN::la']]],
-  ['matrixview_2',['MatrixView',['../structNeoN_1_1la_1_1MatrixView.html',1,'NeoN::la']]],
-  ['meshparams_3',['MeshParams',['../structNeoN_1_1detail_1_1MeshParams.html',1,'NeoN::detail']]]
+  ['matrix_3c_20valuetype_2c_20coosparsitypattern_3c_20linearsystemindextype_20_3e_20_3e_1',['Matrix&lt; ValueType, CooSparsityPattern&lt; LinearSystemIndexType &gt; &gt;',['../classNeoN_1_1la_1_1Matrix.html',1,'NeoN::la']]],
+  ['matrix_3c_20valuetype_2c_20localidx_20_3e_2',['Matrix&lt; ValueType, localIdx &gt;',['../classNeoN_1_1la_1_1Matrix.html',1,'NeoN::la']]],
+  ['matrixview_3',['MatrixView',['../structNeoN_1_1la_1_1MatrixView.html',1,'NeoN::la']]],
+  ['meshparams_4',['MeshParams',['../structNeoN_1_1detail_1_1MeshParams.html',1,'NeoN::detail']]]
 ];
