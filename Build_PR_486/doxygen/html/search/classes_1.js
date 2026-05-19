72e99e4a9ca3c768fea056461f475d3090e299f0
greole <greole@users.noreply.github.com>
2026-05-19 12:15:28 +0000

    Deploying to gh-pages from @ exasim-project/NeoN@3d303239d07e35ea381bf6ed544c59dac0e07146 🚀

--- Build_PR_486/doxygen/html/search/classes_1.js
+++ Build_PR_486/doxygen/html/search/classes_1.js
@@ -11,7 +11,6 @@ var searchData=
   ['boundarydata_3c_20neon_3a_3ascalar_20_3e_8',['BoundaryData&lt; NeoN::scalar &gt;',['../classNeoN_1_1BoundaryData.html',1,'NeoN']]],
   ['boundarydata_3c_20neon_3a_3avec3_20_3e_9',['BoundaryData&lt; NeoN::Vec3 &gt;',['../classNeoN_1_1BoundaryData.html',1,'NeoN']]],
   ['boundarydata_3c_20scalar_20_3e_10',['BoundaryData&lt; scalar &gt;',['../classNeoN_1_1BoundaryData.html',1,'NeoN']]],
-  ['boundarydata_3c_20valuetype_20_3e_11',['BoundaryData&lt; ValueType &gt;',['../classNeoN_1_1BoundaryData.html',1,'NeoN']]],
-  ['boundarymesh_12',['BoundaryMesh',['../classNeoN_1_1BoundaryMesh.html',1,'NeoN']]],
-  ['boundarypatchmixin_13',['BoundaryPatchMixin',['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1BoundaryPatchMixin.html',1,'NeoN::finiteVolume::cellCentred']]]
+  ['boundarymesh_11',['BoundaryMesh',['../classNeoN_1_1BoundaryMesh.html',1,'NeoN']]],
+  ['boundarypatchmixin_12',['BoundaryPatchMixin',['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1BoundaryPatchMixin.html',1,'NeoN::finiteVolume::cellCentred']]]
 ];
