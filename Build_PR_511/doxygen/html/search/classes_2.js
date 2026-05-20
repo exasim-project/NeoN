bbbe5c92ef316808cb1a3ad0546e49548f838ad7
greole <greole@users.noreply.github.com>
2026-05-20 08:33:43 +0000

    Deploying to gh-pages from @ exasim-project/NeoN@1321520baed79b535701caca3cfea58d8c763875 🚀

---
+++ Build_PR_511/doxygen/html/search/classes_2.js
@@ -0,0 +1,15 @@
+var searchData=
+[
+  ['calculated_0',['calculated',['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1surfaceBoundary_1_1Calculated.html',1,'NeoN::finiteVolume::cellCentred::surfaceBoundary::Calculated&lt; ValueType &gt;'],['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1volumeBoundary_1_1Calculated.html',1,'NeoN::finiteVolume::cellCentred::volumeBoundary::Calculated&lt; ValueType &gt;']]],
+  ['celldata_1',['CellData',['../structNeoN_1_1detail_1_1CellData.html',1,'NeoN::detail']]],
+  ['celltofacestencil_2',['CellToFaceStencil',['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1CellToFaceStencil.html',1,'NeoN::finiteVolume::cellCentred']]],
+  ['coeff_3',['Coeff',['../classNeoN_1_1dsl_1_1Coeff.html',1,'NeoN::dsl']]],
+  ['collection_4',['Collection',['../classNeoN_1_1Collection.html',1,'NeoN']]],
+  ['collectionmixin_5',['CollectionMixin',['../classNeoN_1_1CollectionMixin.html',1,'NeoN']]],
+  ['collectionmixin_3c_20oldtimedocument_20_3e_6',['CollectionMixin&lt; OldTimeDocument &gt;',['../classNeoN_1_1CollectionMixin.html',1,'NeoN']]],
+  ['collectionmixin_3c_20vectordocument_20_3e_7',['CollectionMixin&lt; VectorDocument &gt;',['../classNeoN_1_1CollectionMixin.html',1,'NeoN']]],
+  ['coosparsitypattern_8',['CooSparsityPattern',['../classNeoN_1_1la_1_1CooSparsityPattern.html',1,'NeoN::la']]],
+  ['cpuexecutor_9',['CPUExecutor',['../classNeoN_1_1CPUExecutor.html',1,'NeoN']]],
+  ['createfromexistingvector_10',['CreateFromExistingVector',['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1CreateFromExistingVector.html',1,'NeoN::finiteVolume::cellCentred']]],
+  ['csrsparsitypattern_11',['CsrSparsityPattern',['../classNeoN_1_1la_1_1CsrSparsityPattern.html',1,'NeoN::la']]]
+];
---
+++ Build_PR_511/doxygen/html/search/functions_7.js
@@ -0,0 +1,11 @@
+var searchData=
+[
+  ['hasdatabase_0',['hasDatabase',['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1FieldDatabaseMixin.html#af32154beffbe1ae43b714bf44e79551a',1,'NeoN::finiteVolume::cellCentred::FieldDatabaseMixin']]],
+  ['hasid_1',['hasId',['../namespaceNeoN.html#a1515bc3e21ebafd40f42151ea50ed85b',1,'NeoN']]],
+  ['hasoperator_2',['hasOperator',['../classNeoN_1_1dsl_1_1Expression.html#a92424d6e14a1aa5f25db658407a136c3',1,'NeoN::dsl::Expression']]],
+  ['hasoperatoroftype_3',['hasOperatorOfType',['../classNeoN_1_1dsl_1_1Expression.html#a270a19070e2129d7b1eb986a47c4b99c',1,'NeoN::dsl::Expression']]],
+  ['hasscalar_4',['hasScalar',['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1BoundaryContext.html#ad0ac5f05bb32ed8842541fa7314bd85d',1,'NeoN::finiteVolume::cellCentred::BoundaryContext']]],
+  ['hastensor_5',['hasTensor',['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1BoundaryContext.html#a19e3e828bd6e8cef43ac924813317833',1,'NeoN::finiteVolume::cellCentred::BoundaryContext']]],
+  ['hasvector_6',['hasVector',['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1BoundaryContext.html#a2a2d43faccc08fea3ceae00160ab436c',1,'NeoN::finiteVolume::cellCentred::BoundaryContext']]],
+  ['hasview_7',['hasView',['../classNeoN_1_1dsl_1_1Coeff.html#ac081d961b60c3771efca69156b140d3f',1,'NeoN::dsl::Coeff']]]
+];
