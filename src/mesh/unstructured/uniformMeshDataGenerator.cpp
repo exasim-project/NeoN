f5a899a8e85e5fccff15658e8624eff6b92d27f8
chihtaw <wangchihta@gmail.com>
2026-05-29 12:06:51 +0200

    Remove the storage of boundary face owners from unstructured mesh

--- src/mesh/unstructured/uniformMeshDataGenerator.cpp
+++ src/mesh/unstructured/uniformMeshDataGenerator.cpp
@@ -53,7 +53,8 @@ generateInternalFaces(const MeshParams& p, const localIdx nInternalFaces, const 
     std::vector<Vec3> fAreas(static_cast<size_t>(nFaces));
     std::vector<Vec3> fCenters(static_cast<size_t>(nFaces));
     std::vector<scalar> fMag(static_cast<size_t>(nFaces));
-    std::vector<label> fOwner(static_cast<size_t>(nFaces));
+    // std::vector<label> fOwner(static_cast<size_t>(nFaces));
+    std::vector<label> fOwner(static_cast<size_t>(nInternalFaces));
     std::vector<label> fNeighbour(static_cast<size_t>(nInternalFaces));
 
     localIdx faceId = 0;
@@ -165,7 +166,7 @@ BoundaryMesh generateBoundaryData(
         // faces.normals[fi] = faceNormal;
         // faces.centers[fi] = faceCenter;
         // faces.areas[fi] = magA;
-        faces.owner[fi] = ciLabel;
+        // faces.owner[fi] = ciLabel;
 
         bndFaceOwners[sz] = ciLabel;
         bndFaceCenters[sz] = faceCenter;
