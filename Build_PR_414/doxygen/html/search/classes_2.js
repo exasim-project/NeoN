71797e40435abae11a0eda26239a00b19b01544a
greole <greole@users.noreply.github.com>
2026-05-30 15:24:43 +0000

    Deploying to gh-pages from @ exasim-project/NeoN@7a5650c723edbf8ff6cb064fcb5c30346d0bd9bb 🚀

--- Build_PR_414/doxygen/html/search/classes_2.js
+++ Build_PR_414/doxygen/html/search/classes_2.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['calculated_0',['calculated',['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1volumeBoundary_1_1Calculated.html',1,'NeoN::finiteVolume::cellCentred::volumeBoundary::Calculated&lt; ValueType &gt;'],['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1surfaceBoundary_1_1Calculated.html',1,'NeoN::finiteVolume::cellCentred::surfaceBoundary::Calculated&lt; ValueType &gt;']]],
+  ['calculated_0',['calculated',['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1surfaceBoundary_1_1Calculated.html',1,'NeoN::finiteVolume::cellCentred::surfaceBoundary::Calculated&lt; ValueType &gt;'],['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1volumeBoundary_1_1Calculated.html',1,'NeoN::finiteVolume::cellCentred::volumeBoundary::Calculated&lt; ValueType &gt;']]],
   ['cellbaseddata_1',['CellBasedData',['../structNeoN_1_1la_1_1CellBasedIterator_1_1CellBasedData.html',1,'NeoN::la::CellBasedIterator']]],
   ['cellbasediterator_2',['CellBasedIterator',['../classNeoN_1_1la_1_1CellBasedIterator.html',1,'NeoN::la']]],
   ['celldata_3',['CellData',['../structNeoN_1_1detail_1_1CellData.html',1,'NeoN::detail']]],
@@ -10,10 +10,9 @@ var searchData=
   ['collectionmixin_7',['CollectionMixin',['../classNeoN_1_1CollectionMixin.html',1,'NeoN']]],
   ['collectionmixin_3c_20oldtimedocument_20_3e_8',['CollectionMixin&lt; OldTimeDocument &gt;',['../classNeoN_1_1CollectionMixin.html',1,'NeoN']]],
   ['collectionmixin_3c_20vectordocument_20_3e_9',['CollectionMixin&lt; VectorDocument &gt;',['../classNeoN_1_1CollectionMixin.html',1,'NeoN']]],
-  ['communicationpattern_10',['CommunicationPattern',['../structNeoN_1_1CommunicationPattern.html',1,'NeoN']]],
-  ['coosparsitypattern_11',['CooSparsityPattern',['../classNeoN_1_1la_1_1CooSparsityPattern.html',1,'NeoN::la']]],
-  ['corrected_12',['Corrected',['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1Corrected.html',1,'NeoN::finiteVolume::cellCentred']]],
-  ['cpuexecutor_13',['CPUExecutor',['../classNeoN_1_1CPUExecutor.html',1,'NeoN']]],
-  ['createfromexistingvector_14',['CreateFromExistingVector',['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1CreateFromExistingVector.html',1,'NeoN::finiteVolume::cellCentred']]],
-  ['csrsparsitypattern_15',['CsrSparsityPattern',['../classNeoN_1_1la_1_1CsrSparsityPattern.html',1,'NeoN::la']]]
+  ['coosparsitypattern_10',['CooSparsityPattern',['../classNeoN_1_1la_1_1CooSparsityPattern.html',1,'NeoN::la']]],
+  ['corrected_11',['Corrected',['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1Corrected.html',1,'NeoN::finiteVolume::cellCentred']]],
+  ['cpuexecutor_12',['CPUExecutor',['../classNeoN_1_1CPUExecutor.html',1,'NeoN']]],
+  ['createfromexistingvector_13',['CreateFromExistingVector',['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1CreateFromExistingVector.html',1,'NeoN::finiteVolume::cellCentred']]],
+  ['csrsparsitypattern_14',['CsrSparsityPattern',['../classNeoN_1_1la_1_1CsrSparsityPattern.html',1,'NeoN::la']]]
 ];
--- Build_PR_414/doxygen/html/search/variables_8.js
+++ Build_PR_414/doxygen/html/search/variables_8.js
@@ -2,7 +2,8 @@ var searchData=
 [
   ['level_0',['level',['../classNeoN_1_1Logging_1_1LogEvent.html#abc7fe8715a0dc371fb37f27e0e5a9a1e',1,'NeoN::Logging::LogEvent']]],
   ['location_1',['location',['../classNeoN_1_1Logging_1_1LogEvent.html#acbcd09de99ef6864937f7367d73eafdc',1,'NeoN::Logging::LogEvent']]],
-  ['lx_2',['Lx',['../structNeoN_1_1detail_1_1MeshParams.html#acca6fb1b0a1ad9093ce541eb3238d5e7',1,'NeoN::detail::MeshParams']]],
-  ['ly_3',['Ly',['../structNeoN_1_1detail_1_1MeshParams.html#a3a54c2a75a2808618da9fda37a32b3fe',1,'NeoN::detail::MeshParams']]],
-  ['lz_4',['Lz',['../structNeoN_1_1detail_1_1MeshParams.html#a905d93fcf475c523e19cf645f060ac19',1,'NeoN::detail::MeshParams']]]
+  ['ls_2',['ls',['../namespaceNeoN_1_1la.html#a5a1362b264d0ddad8fbcf4d2a203f089',1,'NeoN::la']]],
+  ['lx_3',['Lx',['../structNeoN_1_1detail_1_1MeshParams.html#acca6fb1b0a1ad9093ce541eb3238d5e7',1,'NeoN::detail::MeshParams']]],
+  ['ly_4',['Ly',['../structNeoN_1_1detail_1_1MeshParams.html#a3a54c2a75a2808618da9fda37a32b3fe',1,'NeoN::detail::MeshParams']]],
+  ['lz_5',['Lz',['../structNeoN_1_1detail_1_1MeshParams.html#a905d93fcf475c523e19cf645f060ac19',1,'NeoN::detail::MeshParams']]]
 ];
