14af35e8d09117e4a4e3b8f27af44c9f59220667
greole <greole@users.noreply.github.com>
2026-05-30 21:18:48 +0000

    Deploying to gh-pages from @ exasim-project/NeoN@a83e89051d328b400fd85e6b667d7df093ae9bbd 🚀

---
+++ Build_PR_531/doxygen/html/search/variables_2.js
@@ -0,0 +1,11 @@
+var searchData=
+[
+  ['cellbasediteratordata_5f_0',['cellBasedIteratorData_',['../classNeoN_1_1la_1_1CellBasedIterator.html#a4017259e8843187c956e846ae4ebcc47',1,'NeoN::la::CellBasedIterator']]],
+  ['cellfaces_1',['cellFaces',['../structNeoN_1_1la_1_1CellBasedIterator_1_1CellBasedData.html#a30829c4df80afa9bfe9ebf5993d6141b',1,'NeoN::la::CellBasedIterator::CellBasedData']]],
+  ['centers_2',['centers',['../structNeoN_1_1detail_1_1CellData.html#ab0b25763674ccfa97d6e305901e5b2af',1,'NeoN::detail::CellData::centers'],['../structNeoN_1_1detail_1_1FaceData.html#ad394d054dc71d948a402802f7a98966f',1,'NeoN::detail::FaceData::centers']]],
+  ['coeffs_5f_3',['coeffs_',['../classNeoN_1_1dsl_1_1OperatorMixin.html#aca2e5d4a971d6f5e412d9c3466f0890c',1,'NeoN::dsl::OperatorMixin']]],
+  ['colidxs_4',['colIdxs',['../structNeoN_1_1la_1_1SparsityView.html#a1cb5b4b48a9ed148cf8b403dac414c0e',1,'NeoN::la::SparsityView']]],
+  ['cols_5',['cols',['../structNeoN_1_1la_1_1Dimensions.html#a9f9d5717f4f139b7efc4b6a9a087eb7b',1,'NeoN::la::Dimensions']]],
+  ['control_5fdict_5fname_6',['CONTROL_DICT_NAME',['../classNeoN_1_1Time.html#ad5a35398d9c27636b32a69aa19db11e2',1,'NeoN::Time']]],
+  ['creationts_7',['creationTS',['../classNeoN_1_1Logging_1_1LogEvent.html#ae08d532e07e1a1403f395756b2d07a68',1,'NeoN::Logging::LogEvent']]]
+];
