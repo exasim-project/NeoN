ab8928636cf60d66b18a4885e870630b9730f5a7
Gregor Olenik <gregor.olenik@web.de>
2026-05-30 23:23:18 +0200

    cleanup

--- include/NeoN/core/initialization.hpp
+++ include/NeoN/core/initialization.hpp
@@ -11,15 +11,20 @@
 
 #include <cpptrace/cpptrace.hpp>
 
+#ifdef NF_WITH_MPI_SUPPORT
+#include "NeoN/core/mpi/environment.hpp"
+#endif
+
 namespace NeoN
 {
 
+#ifdef NF_WITH_MPI_SUPPORT
 inline void initialize(int argc, char* argv[])
 {
-    Kokkos::initialize(argc, argv);
+    Kokkos::initialize(argc, argv).set_print_configuration(true).set_map_device_id_by("mpi_rank");
+
     cpptrace::register_terminate_handler();
     mpi::Environment mpiEnviron;
-
     Logging::setNeonDefaultPattern(mpiEnviron);
 }
 
@@ -28,4 +33,19 @@ inline void finalize()
     Logging::info("Finalizing NeoN");
     Kokkos::finalize();
 }
+#else
+inline void initialize(int argc, char* argv[])
+{
+    Kokkos::initialize(argc, argv).set_print_configuration(true);
+    Logging::setNeonDefaultPattern(mpiEnviron);
+}
+
+inline void finalize()
+{
+    Logging::info("Finalizing NeoN");
+    Kokkos::finalize();
+}
+#endif
+
+
 }
