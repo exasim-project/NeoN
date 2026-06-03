e0504e3c12c79a4eb64c500f18bef585a064c983
HendriceH <HendriceH@users.noreply.github.com>
2026-06-03 09:53:23 +0000

    Deploying to gh-pages from @ exasim-project/NeoN@56316ef6f28d8b3643c96ac8441a0144127f6d63 🚀

--- Build_PR_531/doxygen/html/search/namespaces_0.js
+++ Build_PR_531/doxygen/html/search/namespaces_0.js
@@ -16,5 +16,6 @@ var searchData=
   ['neon_3a_3afinitevolume_3a_3acellcentred_3a_3avolumeboundary_3a_3adetail_13',['detail',['../namespaceNeoN_1_1finiteVolume_1_1cellCentred_1_1volumeBoundary_1_1detail.html',1,'NeoN::finiteVolume::cellCentred::volumeBoundary']]],
   ['neon_3a_3ala_14',['la',['../namespaceNeoN_1_1la.html',1,'NeoN']]],
   ['neon_3a_3alogging_15',['Logging',['../namespaceNeoN_1_1Logging.html',1,'NeoN']]],
-  ['neon_3a_3atimeintegration_16',['timeIntegration',['../namespaceNeoN_1_1timeIntegration.html',1,'NeoN']]]
+  ['neon_3a_3alogging_3a_3adetail_16',['detail',['../namespaceNeoN_1_1Logging_1_1detail.html',1,'NeoN::Logging']]],
+  ['neon_3a_3atimeintegration_17',['timeIntegration',['../namespaceNeoN_1_1timeIntegration.html',1,'NeoN']]]
 ];
--- Build_PR_531/doxygen/html/search/variables_5.js
+++ Build_PR_531/doxygen/html/search/variables_5.js
@@ -7,5 +7,6 @@ var searchData=
   ['field_5f_4',['field_',['../classNeoN_1_1dsl_1_1OperatorMixin.html#a0de3b4d1a53e4d2f2da658917d6addbf',1,'NeoN::dsl::OperatorMixin::field_'],['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1DomainMixin.html#afef01a21b9579a76855ce85e318e2cab',1,'NeoN::finiteVolume::cellCentred::DomainMixin::field_']]],
   ['fieldcollectionname_5',['fieldCollectionName',['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1FieldDatabaseMixin.html#a08e0e8c5a92c42724988f72bb7e6cb42',1,'NeoN::finiteVolume::cellCentred::FieldDatabaseMixin']]],
   ['finalresnorm_6',['finalResNorm',['../structNeoN_1_1la_1_1SolverStatsEntry.html#a026a040331e54ab553c8f44fc3357b55',1,'NeoN::la::SolverStatsEntry']]],
-  ['fixesvalue_7',['fixesValue',['../structNeoN_1_1finiteVolume_1_1cellCentred_1_1BoundaryAttributes.html#a6ca8948d47e0dfa00ff58b533d830b2c',1,'NeoN::finiteVolume::cellCentred::BoundaryAttributes']]]
+  ['fixesvalue_7',['fixesValue',['../structNeoN_1_1finiteVolume_1_1cellCentred_1_1BoundaryAttributes.html#a6ca8948d47e0dfa00ff58b533d830b2c',1,'NeoN::finiteVolume::cellCentred::BoundaryAttributes']]],
+  ['fmt_8',['fmt',['../structNeoN_1_1Logging_1_1detail_1_1FatalFormat.html#ac0c9b58e3f9aecefa089a551162bff0a',1,'NeoN::Logging::detail::FatalFormat']]]
 ];
