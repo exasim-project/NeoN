77fbbeda3b75f74802d8e98e4a37000793ddec85
HendriceH <HendriceH@users.noreply.github.com>
2026-06-04 10:37:43 +0000

    Deploying to gh-pages from @ exasim-project/NeoN@ccccbbd46bf9b1f9c54824a542eca8ec0d6c9934 🚀

--- Build_PR_528/doxygen/html/search/classes_5.js
+++ Build_PR_528/doxygen/html/search/classes_5.js
@@ -7,12 +7,13 @@ var searchData=
   ['facetomatrixaddress_4',['FaceToMatrixAddress',['../classNeoN_1_1la_1_1FaceToMatrixAddress.html',1,'NeoN::la']]],
   ['facetomatrixview_5',['FaceToMatrixView',['../structNeoN_1_1la_1_1FaceToMatrixView.html',1,'NeoN::la']]],
   ['factory_6',['Factory',['../classFactory.html',1,'']]],
-  ['field_7',['Field',['../classNeoN_1_1Field.html',1,'NeoN']]],
-  ['field_3c_20neon_3a_3ascalar_20_3e_8',['Field&lt; NeoN::scalar &gt;',['../classNeoN_1_1Field.html',1,'NeoN']]],
-  ['field_3c_20neon_3a_3avec3_20_3e_9',['Field&lt; NeoN::Vec3 &gt;',['../classNeoN_1_1Field.html',1,'NeoN']]],
-  ['field_3c_20scalar_20_3e_10',['Field&lt; scalar &gt;',['../classNeoN_1_1Field.html',1,'NeoN']]],
-  ['fielddatabasemixin_11',['FieldDatabaseMixin',['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1FieldDatabaseMixin.html',1,'NeoN::finiteVolume::cellCentred']]],
-  ['fixedgradient_12',['FixedGradient',['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1volumeBoundary_1_1FixedGradient.html',1,'NeoN::finiteVolume::cellCentred::volumeBoundary']]],
-  ['fixedvalue_13',['fixedvalue',['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1surfaceBoundary_1_1FixedValue.html',1,'NeoN::finiteVolume::cellCentred::surfaceBoundary::FixedValue&lt; ValueType &gt;'],['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1volumeBoundary_1_1FixedValue.html',1,'NeoN::finiteVolume::cellCentred::volumeBoundary::FixedValue&lt; ValueType &gt;']]],
-  ['forwardeuler_14',['ForwardEuler',['../classNeoN_1_1timeIntegration_1_1ForwardEuler.html',1,'NeoN::timeIntegration']]]
+  ['fatalformat_7',['FatalFormat',['../structNeoN_1_1Logging_1_1detail_1_1FatalFormat.html',1,'NeoN::Logging::detail']]],
+  ['field_8',['Field',['../classNeoN_1_1Field.html',1,'NeoN']]],
+  ['field_3c_20neon_3a_3ascalar_20_3e_9',['Field&lt; NeoN::scalar &gt;',['../classNeoN_1_1Field.html',1,'NeoN']]],
+  ['field_3c_20neon_3a_3avec3_20_3e_10',['Field&lt; NeoN::Vec3 &gt;',['../classNeoN_1_1Field.html',1,'NeoN']]],
+  ['field_3c_20scalar_20_3e_11',['Field&lt; scalar &gt;',['../classNeoN_1_1Field.html',1,'NeoN']]],
+  ['fielddatabasemixin_12',['FieldDatabaseMixin',['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1FieldDatabaseMixin.html',1,'NeoN::finiteVolume::cellCentred']]],
+  ['fixedgradient_13',['FixedGradient',['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1volumeBoundary_1_1FixedGradient.html',1,'NeoN::finiteVolume::cellCentred::volumeBoundary']]],
+  ['fixedvalue_14',['fixedvalue',['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1surfaceBoundary_1_1FixedValue.html',1,'NeoN::finiteVolume::cellCentred::surfaceBoundary::FixedValue&lt; ValueType &gt;'],['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1volumeBoundary_1_1FixedValue.html',1,'NeoN::finiteVolume::cellCentred::volumeBoundary::FixedValue&lt; ValueType &gt;']]],
+  ['forwardeuler_15',['ForwardEuler',['../classNeoN_1_1timeIntegration_1_1ForwardEuler.html',1,'NeoN::timeIntegration']]]
 ];
