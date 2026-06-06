b9f93bc86338e63c5cc514fa510d409e1e5bc560
greole <greole@users.noreply.github.com>
2026-06-06 20:04:29 +0000

    Deploying to gh-pages from @ exasim-project/NeoN@df27191ab27242ba58cc28c58991b942390e3641 🚀

--- Build_PR_528/doxygen/html/menudata.js
+++ Build_PR_528/doxygen/html/menudata.js
@@ -156,7 +156,8 @@ var menudata={children:[
 {text:"p",url:"functions_type.html#index_p"},
 {text:"s",url:"functions_type.html#index_s"},
 {text:"v",url:"functions_type.html#index_v"}]},
-{text:"Enumerations",url:"functions_enum.html"}]}]},
+{text:"Enumerations",url:"functions_enum.html"},
+{text:"Related Symbols",url:"functions_rela.html"}]}]},
 {text:"Files",url:"files.html",children:[
 {text:"File List",url:"files.html"},
 {text:"File Members",url:"globals.html",children:[
