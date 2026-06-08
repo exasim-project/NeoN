6c8078dcaee4c1c07ad61eebcbdef1e9f75eea5c
HendriceH <HendriceH@users.noreply.github.com>
2026-06-08 14:10:51 +0000

    Deploying to gh-pages from @ exasim-project/NeoN@f8f17c50627237c47c45da56965829b6971bd9bd 🚀

--- Build_PR_528/doxygen/html/search/files_d.js
+++ Build_PR_528/doxygen/html/search/files_d.js
@@ -5,6 +5,5 @@ var searchData=
   ['petsc_2ehpp_2',['petsc.hpp',['../petsc_8hpp.html',1,'']]],
   ['petscsolvercontext_2ehpp_3',['petscSolverContext.hpp',['../petscSolverContext_8hpp.html',1,'']]],
   ['processor_2ehpp_4',['processor.hpp',['../surface_2processor_8hpp.html',1,'(Global Namespace)'],['../volume_2processor_8hpp.html',1,'(Global Namespace)']]],
-  ['processorgradienthalo_2ehpp_5',['processorGradientHalo.hpp',['../processorGradientHalo_8hpp.html',1,'']]],
-  ['procfacesymmetryprobe_2ehpp_6',['procFaceSymmetryProbe.hpp',['../procFaceSymmetryProbe_8hpp.html',1,'']]]
+  ['processorgradienthalo_2ehpp_5',['processorGradientHalo.hpp',['../processorGradientHalo_8hpp.html',1,'']]]
 ];
