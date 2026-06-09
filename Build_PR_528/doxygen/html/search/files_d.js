1b404b0b90e8059d0432597d84609fb279b7b886
HendriceH <HendriceH@users.noreply.github.com>
2026-06-09 09:47:26 +0000

    Deploying to gh-pages from @ exasim-project/NeoN@d6a347e1cd21958e29f74e3c4f10cd6bc616571d 🚀

--- Build_PR_528/doxygen/html/search/files_d.js
+++ Build_PR_528/doxygen/html/search/files_d.js
@@ -4,6 +4,5 @@ var searchData=
   ['partitioning_2ehpp_1',['partitioning.hpp',['../partitioning_8hpp.html',1,'']]],
   ['petsc_2ehpp_2',['petsc.hpp',['../petsc_8hpp.html',1,'']]],
   ['petscsolvercontext_2ehpp_3',['petscSolverContext.hpp',['../petscSolverContext_8hpp.html',1,'']]],
-  ['processor_2ehpp_4',['processor.hpp',['../surface_2processor_8hpp.html',1,'(Global Namespace)'],['../volume_2processor_8hpp.html',1,'(Global Namespace)']]],
-  ['processorgradienthalo_2ehpp_5',['processorGradientHalo.hpp',['../processorGradientHalo_8hpp.html',1,'']]]
+  ['processor_2ehpp_4',['processor.hpp',['../surface_2processor_8hpp.html',1,'(Global Namespace)'],['../volume_2processor_8hpp.html',1,'(Global Namespace)']]]
 ];
