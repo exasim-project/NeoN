eb4d3621b3229183e4b2a9b906d9d7f51c14629f
Gregor Olenik <gregor.olenik@web.de>
2026-06-13 12:42:31 +0200

    use multiple RHS with L1

--- src/linearAlgebra/ginkgo/ginkgo.cpp
+++ src/linearAlgebra/ginkgo/ginkgo.cpp
@@ -520,32 +520,39 @@ SolverStats GinkgoSolver::solve(
 }
 
 
-template<unsigned int I>
-void solveVec3RhsComponent(
-    const Vector<Vec3>& rhs,
-    Vector<Vec3>& x,
-    std::shared_ptr<const gko::Executor> exec,
-    std::shared_ptr<const gko::LinOpFactory> factory,
-    std::shared_ptr<const gko::LinOp> gkoMtx,
-    SolverStats& stats,
-    const L1ResidualControl* l1Control
-)
-{
-    auto rhsComp = getComponent<I>(rhs);
-    auto xcopy = getComponent<I>(x);
-    stats.entries.push_back(
-        solve_impl(exec, rhsComp, xcopy, gkoMtx, factory->generate(gkoMtx), l1Control)
-    );
-    setComponent<I>(xcopy, x);
-}
-
-
 SolverStats GinkgoSolver::solve(
     const LinearSystem<scalar, Vec3, CSRMatrix<scalar, localIdx>, COOMatrix<scalar, localIdx>>& sys,
     Vector<Vec3>& x
 ) const
 {
     auto gkoMtx = createGkoMtx(sys.matrix());
+    const L1ResidualControl* l1Control = l1Control_ ? &l1Control_.value() : nullptr;
+    if (l1Control)
+    {
+        gkoExec_->synchronize();
+        auto startEval = std::chrono::steady_clock::now();
+
+        label nrows = sys.rhs().size();
+        const auto b = gkoVecView(gkoExec_, sys.rhs().data(), nrows); // [nrows x 3]
+        auto xView = gkoVecView(gkoExec_, x.data(), nrows);           // [nrows x 3]
+        auto solver = factory_->generate(gkoMtx);
+        auto l1Res = solveWithL1Stop(gkoExec_, gkoMtx, b, xView, solver.get(), *l1Control);
+
+        gkoExec_->synchronize();
+        auto endEval = std::chrono::steady_clock::now();
+        auto duration =
+            static_cast<scalar>(
+                std::chrono::duration_cast<std::chrono::microseconds>(endEval - startEval).count()
+            )
+            / 1000.0;
+
+        SolverStats stats;
+        for (int i = 0; i < 3; ++i)
+            stats.entries.push_back(
+                {l1Res.numIter, l1Res.perColInitNorms[i], l1Res.perColFinalNorms[i], duration}
+            );
+        return stats;
+    }
     return solve_impl(gkoExec_, sys.rhs(), x, gkoMtx, factory_->generate(gkoMtx));
 }
 
