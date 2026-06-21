606f32301a04087855530597fc39802389bd09b4
greole <greole@users.noreply.github.com>
2026-06-21 10:53:24 +0000

    Deploying to gh-pages from @ exasim-project/NeoN@98f356437af7fbc6865ffffeb9f9530c52fa5914 🚀

---
+++ Build_PR_548/doxygen/html/search/all_9.js
@@ -0,0 +1,9 @@
+var searchData=
+[
+  ['keepfacefluxcorrection_0',['keepfacefluxcorrection',['../classNeoN_1_1la_1_1LinearSystem.html#a832ecda24e344d7e34c79029d0d4827e',1,'NeoN::la::LinearSystem::keepFaceFluxCorrection() const'],['../classNeoN_1_1la_1_1LinearSystem.html#a0de166fc56197b36ad0fc9ab59c59ddf',1,'NeoN::la::LinearSystem::keepFaceFluxCorrection(bool keep)']]],
+  ['key_1',['key',['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1FieldDatabaseMixin.html#ab1ea0a9e16f0d7fdfe345d755b069d30',1,'NeoN::finiteVolume::cellCentred::FieldDatabaseMixin']]],
+  ['keys_2',['keys',['../classNeoN_1_1Dictionary.html#ab7a9dfa55b6449147a8115f107771e88',1,'NeoN::Dictionary']]],
+  ['kokkos_2ehpp_3',['kokkos.hpp',['../kokkos_8hpp.html',1,'']]],
+  ['kokkosallocactor_4',['KokkosAllocactor',['../classKokkosAllocactor.html',1,'']]],
+  ['kokkosallocator_5',['KokkosAllocator',['../classNeoN_1_1KokkosAllocator.html',1,'NeoN']]]
+];
