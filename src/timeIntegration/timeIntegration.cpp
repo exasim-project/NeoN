238db2d7c79d8cbff4b2e1d09bfb41bdb46aa588
Gregor Olenik <gregor.olenik@web.de>
2026-06-24 06:59:14 +0200

    add steady-state

--- src/timeIntegration/timeIntegration.cpp
+++ src/timeIntegration/timeIntegration.cpp
@@ -5,6 +5,7 @@
 #include "NeoN/timeIntegration/timeIntegration.hpp"
 #include "NeoN/timeIntegration/forwardEuler.hpp"
 #include "NeoN/timeIntegration/backwardEuler.hpp"
+#include "NeoN/timeIntegration/steadyState.hpp"
 
 namespace fvcc = NeoN::finiteVolume::cellCentred;
 
@@ -16,5 +17,8 @@ template class ForwardEuler<fvcc::VolumeField<Vec3>>;
 
 template class BackwardEuler<fvcc::VolumeField<scalar>>;
 template class BackwardEuler<fvcc::VolumeField<Vec3>>;
+
+template class SteadyState<fvcc::VolumeField<scalar>>;
+template class SteadyState<fvcc::VolumeField<Vec3>>;
 
 } // namespace NeoN::dsl
