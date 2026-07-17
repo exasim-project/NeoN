b790fb70f2428f455998deee528909717c36b448
chihtaw <wangchihta@gmail.com>
2026-07-17 15:23:57 +0200

    Fix the type for arguments of surfaceIntegrate function

--- include/NeoN/finiteVolume/cellCentred/operators/surfaceIntegrate.hpp
+++ include/NeoN/finiteVolume/cellCentred/operators/surfaceIntegrate.hpp
@@ -18,9 +18,9 @@ template<typename ValueType>
 void surfaceIntegrate(
     const Executor& exec,
     localIdx nInternalFaces,
-    View<const int> internalFaceNeighbors,
-    View<const int> internalFaceOwners,
-    View<const int> boundaryFaceOwners,
+    View<const label> internalFaceNeighbors,
+    View<const label> internalFaceOwners,
+    View<const label> boundaryFaceOwners,
     View<const ValueType> flux,
     View<const ValueType> bFlux,
     View<const scalar> v,
