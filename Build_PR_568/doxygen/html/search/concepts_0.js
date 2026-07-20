9398b36d7db5f9bf5e1f7576c88a2d217462f9c0
dheerajraghunathan <dheerajraghunathan@users.noreply.github.com>
2026-07-20 20:20:13 +0000

    Deploying to gh-pages from @ exasim-project/NeoN@fe644c4d5a0b4abec36a925a9928a5356093f02c 🚀

--- Build_PR_568/doxygen/html/search/concepts_0.js
+++ Build_PR_568/doxygen/html/search/concepts_0.js
@@ -6,10 +6,11 @@ var searchData=
   ['neon_3a_3adsl_3a_3ahasimplicitoperatorscalarmtx_3',['HasImplicitOperatorScalarMtx',['../conceptNeoN_1_1dsl_1_1HasImplicitOperatorScalarMtx.html',1,'NeoN::dsl']]],
   ['neon_3a_3adsl_3a_3ahastemporalexplicitoperator_4',['HasTemporalExplicitOperator',['../conceptNeoN_1_1dsl_1_1HasTemporalExplicitOperator.html',1,'NeoN::dsl']]],
   ['neon_3a_3adsl_3a_3ahastemporalimplicitoperator_5',['HasTemporalImplicitOperator',['../conceptNeoN_1_1dsl_1_1HasTemporalImplicitOperator.html',1,'NeoN::dsl']]],
-  ['neon_3a_3adsl_3a_3ahastemporalimplicitoperatorscalarmtx_6',['HasTemporalImplicitOperatorScalarMtx',['../conceptNeoN_1_1dsl_1_1HasTemporalImplicitOperatorScalarMtx.html',1,'NeoN::dsl']]],
-  ['neon_3a_3adsl_3a_3ahastemporaloperator_7',['HasTemporalOperator',['../conceptNeoN_1_1dsl_1_1HasTemporalOperator.html',1,'NeoN::dsl']]],
-  ['neon_3a_3adsl_3a_3aisspatialoperator_8',['IsSpatialOperator',['../conceptNeoN_1_1dsl_1_1IsSpatialOperator.html',1,'NeoN::dsl']]],
-  ['neon_3a_3ahasview_9',['hasView',['../conceptNeoN_1_1hasView.html',1,'NeoN']]],
-  ['neon_3a_3aparallelforcontainerkernel_10',['parallelForContainerKernel',['../conceptNeoN_1_1parallelForContainerKernel.html',1,'NeoN']]],
-  ['neon_3a_3aparallelforkernel_11',['parallelForKernel',['../conceptNeoN_1_1parallelForKernel.html',1,'NeoN']]]
+  ['neon_3a_3adsl_3a_3ahastemporalimplicitoperatorell_6',['HasTemporalImplicitOperatorELL',['../conceptNeoN_1_1dsl_1_1HasTemporalImplicitOperatorELL.html',1,'NeoN::dsl']]],
+  ['neon_3a_3adsl_3a_3ahastemporalimplicitoperatorscalarmtx_7',['HasTemporalImplicitOperatorScalarMtx',['../conceptNeoN_1_1dsl_1_1HasTemporalImplicitOperatorScalarMtx.html',1,'NeoN::dsl']]],
+  ['neon_3a_3adsl_3a_3ahastemporaloperator_8',['HasTemporalOperator',['../conceptNeoN_1_1dsl_1_1HasTemporalOperator.html',1,'NeoN::dsl']]],
+  ['neon_3a_3adsl_3a_3aisspatialoperator_9',['IsSpatialOperator',['../conceptNeoN_1_1dsl_1_1IsSpatialOperator.html',1,'NeoN::dsl']]],
+  ['neon_3a_3ahasview_10',['hasView',['../conceptNeoN_1_1hasView.html',1,'NeoN']]],
+  ['neon_3a_3aparallelforcontainerkernel_11',['parallelForContainerKernel',['../conceptNeoN_1_1parallelForContainerKernel.html',1,'NeoN']]],
+  ['neon_3a_3aparallelforkernel_12',['parallelForKernel',['../conceptNeoN_1_1parallelForKernel.html',1,'NeoN']]]
 ];
