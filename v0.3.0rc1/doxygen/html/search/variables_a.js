32e09050169d3dcdfbc04a447c39bd3b98405fee
greole <gregor.olenik@web.de>
2026-07-30 05:44:37 +0000

    Deploying to gh-pages from @ exasim-project/NeoN@bf354000199ef3f60f782cc347a04103e9286021 🚀

---
+++ v0.3.0rc1/doxygen/html/search/variables_a.js
@@ -0,0 +1,13 @@
+var searchData=
+[
+  ['name_0',['name',['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1CreateFromExistingVector.html#ae8f9c3ba621f5ff0cf743ad317090983',1,'NeoN::finiteVolume::cellCentred::CreateFromExistingVector::name'],['../classNeoN_1_1finiteVolume_1_1cellCentred_1_1DomainMixin.html#adccbbd2c1b3d558ca69c4db20e2f9392',1,'NeoN::finiteVolume::cellCentred::DomainMixin::name']]],
+  ['name_5f_1',['name_',['../classNeoN_1_1CollectionMixin.html#a019b4a98e1b7c10e7657bf5922a0ca3c',1,'NeoN::CollectionMixin']]],
+  ['neighbour_2',['neighbour',['../structNeoN_1_1detail_1_1FaceData.html#a95a9eca3feedc6a0419a5247f3838af6',1,'NeoN::detail::FaceData']]],
+  ['neighbouroffset_3',['neighbourOffset',['../structNeoN_1_1la_1_1FaceToMatrixView.html#ae3737684bb67f8bb28c5d2bc5732ac6b',1,'NeoN::la::FaceToMatrixView']]],
+  ['nl_4',['NL',['../time_8hpp.html#a6c2bdfd6f25ccadb92fcff4173b7fec2',1,'time.hpp']]],
+  ['normals_5',['normals',['../structNeoN_1_1detail_1_1FaceData.html#a725af2dde576e866edb25454a4d9d441',1,'NeoN::detail::FaceData']]],
+  ['numiter_6',['numIter',['../structNeoN_1_1la_1_1SolverStatsEntry.html#a3f5a2b6addcb2a66e3db567419f450f6',1,'NeoN::la::SolverStatsEntry']]],
+  ['nx_7',['nx',['../structNeoN_1_1detail_1_1MeshParams.html#ad9362f60981ecfaa411f793619f7bc82',1,'NeoN::detail::MeshParams']]],
+  ['ny_8',['ny',['../structNeoN_1_1detail_1_1MeshParams.html#a47a2ae7affb90dc1d34c8b950f06de2f',1,'NeoN::detail::MeshParams']]],
+  ['nz_9',['nz',['../structNeoN_1_1detail_1_1MeshParams.html#a70327af024934248c1727eabbdbc719c',1,'NeoN::detail::MeshParams']]]
+];
