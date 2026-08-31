cb4d9e4fbb66094b22435a2c1f1cad7686f1cd24
greole <greole@users.noreply.github.com>
2026-08-31 10:02:36 +0000

    Deploying to gh-pages from @ exasim-project/NeoN@467192e2200d59f7923fe9d619ac16aa38a6f572 🚀

---
+++ Build_PR_589/doxygen/html/search/enumvalues_3.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['error_0',['Error',['../namespaceNeoN_1_1Logging.html#a8cfeee86930e70637e61a759b7361214a66902657569bc8a0384704af92dfb7f3',1,'NeoN::Logging']]],
+  ['explicit_1',['Explicit',['../classNeoN_1_1dsl_1_1Operator.html#ad72823c9a2d04641ab7c7181902f6dd3a9d71760e2dd451db2df354d0fd29cd7f',1,'NeoN::dsl::Operator']]]
+];
