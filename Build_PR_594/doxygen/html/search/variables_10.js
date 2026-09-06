577ffb106a1a371cf260ddd0629df8e7792bd2a2
greole <greole@users.noreply.github.com>
2026-09-06 07:23:00 +0000

    Deploying to gh-pages from @ exasim-project/NeoN@e76ac700c530503ffe74c419729750a94bdc3fe4 🚀

---
+++ Build_PR_594/doxygen/html/search/variables_10.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['values_0',['values',['../classNeoN_1_1SegmentedVectorView.html#ac9d5ff1ed74531fe956a31da40b0565e',1,'NeoN::SegmentedVectorView::values'],['../structNeoN_1_1la_1_1MatrixView.html#a233fe4d9be8c549e05266e85b836a2cc',1,'NeoN::la::MatrixView::values']]],
+  ['volumes_1',['volumes',['../structNeoN_1_1detail_1_1CellData.html#acc2645265e4ac1ac8cbd74f3281de40a',1,'NeoN::detail::CellData']]]
+];
